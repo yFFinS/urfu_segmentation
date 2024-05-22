beb826698178637275b2c624f18825ddc1434ad2
Electrotubbie <steen2033@yandex.ru>
2024-05-23 03:07:23 +0500

    change save pic in aux tools

--- urfu_project/aux_tools.py
+++ urfu_project/aux_tools.py
@@ -4,17 +4,18 @@ from PIL import Image
 from tqdm import tqdm
 
 
-def add_palette_for_1ch_img(path, palette):
+def add_palette_for_1ch_img(path, palette, pic_suffix='.tif'):
     '''
     !!! Функция предназначена для подготовки масок снимков к обучению с помощью MMSegmentation.
-    Функция для создания палтиры у изображения с последующей перезаписью изображения. 
+    Функция для создания палтиры у изображения с последующей перезаписью изображения в формате PNG. 
 
-    path: путь к директории с картинками
+    path: путь к директории с изображениями масок
     palette: палитра цветов у масок
         Пример палитры:
         [[0, 0, 0], [64, 64, 64], [128, 128, 128], [192, 192, 192], [255, 255, 255]]
         для классов:
         ('background', 'water', 'tree', 'roads', 'buildings')
+    pic_suffix='.tif': тип изображения
     '''
     palette_dict = {int(color[0]):i for i, color in enumerate(palette)}
     for img in tqdm(list(os.scandir(path))):
@@ -23,7 +24,7 @@ def add_palette_for_1ch_img(path, palette):
         ar = np.vectorize(palette_dict.get)(ar)
         im = Image.fromarray(ar).convert('P')
         im.putpalette(np.array(palette, dtype=np.uint8))
-        im.save(img.path)
+        im.save(img.path.rstrip(pic_suffix) + '.png')
 
 
 def create_splits(dataset_path, splits_dir_name, imgs_dir_name, pic_count=None, train_size=0.7, pic_suffix=''):
