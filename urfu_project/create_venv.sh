e90fa87f4a731cefbd771011abd2d824060ef3f9
Electrotubbie <steen2033@yandex.ru>
2024-05-22 18:31:18 +0500

    delete torchaudio from create venv

--- urfu_project/create_venv.sh
+++ urfu_project/create_venv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 -m venv .venv # create venv
 source ./.venv/bin/activate # activate venv
-pip3 install torch torchvision torchaudio # torch
+pip3 install torch torchvision # torch
 pip install -U openmim # install mim for install some mm libs
 mim install mmengine # mmengine from mim
 mim install "mmcv>=2.0.0" # mmcv from mim
